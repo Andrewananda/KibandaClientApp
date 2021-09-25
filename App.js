7d85176275bb4568ad58c5dbdb0e77abbd52f040
andrewtiema@gmail.com <andrewtiema@gmail.com>
2021-09-25 13:25:41 +0300

    fixes

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import Splash from './src/splash';
+import Login from './src/login';
 
 const Stack = createStackNavigator();
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
             name={'Screen'}
             component={Splash}
           />
+          <Stack.Screen name={'Login'} component={Login} />
         </Stack.Navigator>
       </NavigationContainer>
     );
