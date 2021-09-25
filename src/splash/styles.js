dfc35184ba8d0347464520d7a49f1ee0e74b1ef2
andrewtiema@gmail.com <andrewtiema@gmail.com>
2021-09-25 12:47:13 +0300

    splash

---
+++ src/splash/styles.js
@@ -0,0 +1,47 @@
+import React,{Component} from 'react';
+import {ActivityIndicator, SafeAreaView, Text, View} from 'react-native';
+import {CommonActions} from '@react-navigation/native';
+import {colors} from '../util/colors';
+
+class Splash extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  redirect(page) {
+    this.props.navigation.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [{name: page}],
+      }),
+    );
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: colors.color_primary,
+            justifyContent: 'center',
+            alignContent: 'center',
+          }}>
+          <Text
+            style={{fontSize: 30, color: colors.white, alignSelf: 'center'}}>
+            Kibanda Client App
+          </Text>
+          <View>
+            <ActivityIndicator
+              size={24}
+              color={colors.white}
+              style={{marginTop: 20}}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default Splash;
---
+++ src/util/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+  color_primary: '#4ba63c',
+  color_secondary: '#0a7607',
+  white: '#ffffff',
+  darkGray: '#c0c0c0',
+  lightGray: '#f6f6f6',
+  blue: 'blue',
+};
